c7dbb389b6ee58e1d3a88dac73ce1872d07f6d8c
Patrick Boe <patrickboe@gmail.com>
2011-11-16 23:09:11 -0500

    check

--- p10.clj
+++ p10.clj
@@ -12,19 +12,32 @@
       (sundaram-row t-limit)
       (range 1 (- (quot t-limit 3) 1)))))
 
-(defn low-rows [table]
-  (lazy-seq
-    (let [r (first table)
-          n (first r)]
-      (if (= 0 n)
-        (list (rest r))
-        (cons r (low-rows (rest table)))))))
+(defn low-row-count
+  ([table] (low-row-count table 0))
+  ([table tally]
+    (let [new-tally (+ 1 tally)]
+      (if (= 0 (first (first table)))
+        new-tally
+        (recur (rest table) new-tally)))))
+
+(defn split-by-consumable [table]
+  (let [[consuming dormant] (split-at (low-row-count table) table)]
+    [(drop-while empty? consuming) dormant]))
 
 (defn firsts [table]
   (map first table))
 
-(defn lowest-n [table]
-  (apply min (firsts (low-rows table))))
+(defn prune-row [value]
+  #(if (= value (first %)) (rest %) %))
 
-(defn next-sundaram [table]
-  (let [n (lowest-n table)]))
+(defn prune [table value]
+  (map (prune-row value) table))
+
+(defn sundaram-series [table]
+  (lazy-seq
+    (let [[consumable dormant] (split-by-consumable table)]
+      (and
+        consumable
+        (let [lowest-n (apply min (firsts consumable))
+              rest-of-table (concat (prune consumable lowest-n) dormant)]
+          (cons lowest-n (sundaram-series rest-of-table)))))))
