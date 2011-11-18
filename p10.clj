e5ade3d0b6c2bd3d397fdc63627ed37107233c48
ncanzone <nicholas.canzoneri@algorithmics.com>
2011-11-18 17:27:20 -0500

    untested first cut at a listing of sundaram

--- p10.clj
+++ p10.clj
@@ -14,23 +14,33 @@
       (range 1 (+ 1 (quot n 3))))))
 
 (defn decreasing? [x]
-  (> (first (x 0)) (first (x 1))))
+  (and (x 1) (> (first (x 0)) (first (x 1)))))
 
 (defn take-bite [table]
-  (let [[unripe [bite]](split-with decreasing? (partition 2 1 table))
+  (let [[upwards downwards] (split-with decreasing? (partition 2 1 table))
+       [unripe [[bite & bitten] & more]] (split-at (count upwards) table)]
+    [bite (concat unripe (if bitten (cons bitten more) more))]))
 
 (defn sundaram-sort [table]
   (lazy-seq
     (let [[k ks] (bite table)
           rest-sorted (sundaram-sort ks)]
-      (or (and k (cons k rest-sorted)) rest-sorted))))
+      (or (and k (cons (to-composite k) rest-sorted)) rest-sorted))))
+
+(defn remove-sundarams [sundarams xs]
+  (lazy-seq
+    (if (= (first xs) (first sundarams))
+      (remove-sundarams (rest sundarams)(rest xs))
+      (cons (first xs) (remove-sundarams (rest sundarams)(rest xs))))))
 
 (defn primes-to [limit]
   (let [n (quot limit 2)
-        numbers (int-array (range 0 n)) 
-        sundaram-numbers (apply concat (sundaram-table n))]
-    (dorun (map #(aset numbers % 0) sundaram-numbers))
-    (cons 2 (map sundaram-to-prime (remove zero? (seq numbers))))))
+        numbers (int-array (range 0 n))]
+    (cons 2
+      (map sundaram-to-prime
+        (remove-sundarams
+          (sundaram-sort (sundaram-table n))
+          numbers)))
 
 (defn sum-primes-to [limit]
   (apply + (primes-to limit)))
