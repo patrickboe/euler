14d27c02df84d7ed2dbf8f4ea461ecfb9ddfe743
Patrick Boe <patrickboe@gmail.com>
2011-11-18 00:17:39 -0500

    working sundaram implementation showing performance improvement over pure functional euler sieve

--- p10.clj
+++ p10.clj
@@ -1,16 +1,20 @@
-(use 'clojure.contrib.math)
+(defn sundaram-to-prime [n] (+ 1 (* 2 n)))
 
-(defn composite-relative [n] (+ 1 (* 2 n)))
+(defn sundaram-row [n]
+  #(range (+ 1 (* % 3)) n (+ 1 (* 2 %))))
 
-(defn sundaram-row [t-limit]
-  #(map composite-relative (range
-     (+ 1 (* % 3))
-     t-limit
-     (+ 1 (* 2 %)))))
+(defn sundaram-table [n]
+  (remove empty?
+    (map
+      (sundaram-row n)
+      (range 1 (+ 1 (quot n 3))))))
 
-(defn sundaram-table [limit]
-  (let [t-limit (quot limit 2)]
-    (remove empty?
-      (map
-        (sundaram-row t-limit)
-        (range 1 (+ 1 (quot t-limit 3)))))))
+(defn primes-to [limit]
+  (let [n (quot limit 2)
+        numbers (int-array (range 0 n)) 
+        sundaram-numbers (apply concat (sundaram-table n))]
+    (dorun (map #(aset numbers % 0) sundaram-numbers))
+    (cons 2 (map sundaram-to-prime (remove zero? (seq numbers))))))
+
+(defn sum-primes-to [limit]
+  (apply + (primes-to limit)))
