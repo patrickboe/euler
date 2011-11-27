dfe9a2b224656c2d51d6f06a615ce5af40540a5b
Patrick Boe <patrickboe@gmail.com>
2011-11-27 08:17:38 -0500

    works but is kind of slow

--- p14.clj
+++ p14.clj
@@ -1,14 +1,11 @@
-(defn greater-than-one? [x] (> x 1))
+(defn non-one? [x] (not (= x 1)))
 
 (defn next-term [k]
-  (if (greater-than-one? k)
+  (if (non-one? k)
     (if
       (even? k)
       (/ k 2)
       (+ 1 (* 3 k)))))
-
-(defn seq-from [n]
-  (take-while identity (iterate next-term n)))
 
 (defn count-from [n]
   (if n
@@ -17,8 +14,16 @@
 
 (def count-from (memoize count-from))
 
-(defn max-count [a b]
-  (if (> (count-from a) (count-from b)) a b))
+(defn max-count [[run-max run-best :as running] k]
+  (let [c (count-from k)]
+    (if
+      (> run-max c)
+      running
+      [c k])))
 
 (defn max-seq-under [n]
-  (reduce max-count 1 (take-while #(< % n) (range 2 n))))
+  (second
+    (reduce
+      max-count
+      [1 1]
+      (range 2 n))))
