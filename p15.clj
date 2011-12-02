7b078a247ca485cc6594da0d50901146de898a61
Patrick Boe <patrickboe@gmail.com>
2011-12-01 22:33:14 -0500

    solved 15

--- p15.clj
+++ p15.clj
@@ -1,36 +1,18 @@
-(:use 'clojure.contrib.graph)
+(defn paths
+  ([m n]
+   (if (>= n m)
+     (if
+       (= 1 m)
+       1
+       (+
+         (paths (- m 1) n)
+         (paths m (- n 1))))
+     (paths n m))))
 
-(defn square [x] (* x x))
+(def paths (memoize paths))
 
-(defn grid [n]
-  (let [r (range 0 (+ 1 n))]
-    (map (fn [i] (map (fn [j] (list i j)) r)) r)))
+(defn square-paths [edge-width]
+  (let [node-width (+ 1 edge-width)]
+    (paths node-width node-width)))
 
-(defn potential-neighbors [node]
-  (list
-    (list (+ (first node) 1) (second node))
-    (list (- (first node) 1) (second node))
-    (list (first node) (+ (second node) 1))
-    (list (first node) (- (second node) 1))))
-
-(defn legal-for [n node]
-  (and
-    (>= (first node) 0)
-    (>= (second node) 0)
-    (>= n (first node))
-    (>= n (second node))))
-
-(defn neighbor-select [n edges]
-  (fn [node]
-    (filter (partial legal-for n) 
-      (potential-neighbors node))))
-
-(defn adjacency-graph
-  ([n]
-   (adjacency-graph
-     n
-     (apply vector-of :boolean (repeat (square (+ 1 n)) true))))
-  ([n edges]
-   (struct directed-graph
-     (grid n)
-     (neighbor-select n edges))))
+(square-paths 20)
