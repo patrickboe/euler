e4e4b32c32984c8ec10df741e3ca68ad82478d02
Patrick Boe <patrickboe@gmail.com>
2012-02-21 22:01:58 -0500

    defined numeric ranges

--- p17.rkt
+++ p17.rkt
@@ -1,1 +1,40 @@
-(def b 1)
+#lang racket
+
+(define num-names
+#hash((1 . "one")
+      (2 . "two")
+      (3 . "three")
+      (4 . "four")
+      (5 . "five")
+      (6 . "six")
+      (7 . "seven")
+      (8 . "eight")
+      (9 . "nine")
+      (10 . "ten")
+      (11 . "eleven")
+      (12 . "twelve")
+      (13 . "thirteen")
+      (14 . "fourteen")
+      (15 . "fifteen")
+      (16 . "sixteen")
+      (17 . "seventeen")
+      (18 . "eighteen")
+      (19 . "nineteen")
+      (20 . "twenty")
+      (30 . "thirty")
+      (40 . "forty")
+      (50 . "fifty")
+      (60 . "sixty")
+      (70 . "seventy")
+      (80 . "eighty")
+      (90 . "ninety")
+      (100 . "hundred")
+      (1000 . "thousand")))
+
+(define digits (in-range 1 10))
+
+(define magnitudes '(100 1000))
+
+(define teens (in-range 10 20))
+
+(define decades (map (lambda (x) (* 10 x)) (rest digits)))
