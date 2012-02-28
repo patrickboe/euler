65fa5c2dcc9be5f9b0f6a3dd1a8323253c854bf0
Patrick Boe <patrickboe@gmail.com>
2012-02-27 22:50:14 -0500

    completed problem 21

--- p21.rkt
+++ p21.rkt
@@ -2,3 +2,23 @@
 
 (require "factors.rkt")
 
+(define (divisor-sums-under n)
+  (stream->list
+    (stream-map
+      (λ(a) (cons a (apply + (proper-divisors a))))
+      (in-range 1 n))))
+
+(define (amicable-nums-under n)
+  (let ([dsums (make-immutable-hash (divisor-sums-under n))])
+    (stream-fold
+      (λ(a x)
+        (if
+          (and
+            (eq? x (hash-ref dsums (hash-ref dsums x) 0))
+            (not (eq? x (hash-ref dsums x))))
+          (cons x a)
+          a))
+      '()
+      (in-range 1 n))))
+
+(apply + (amicable-nums-under 10000))
