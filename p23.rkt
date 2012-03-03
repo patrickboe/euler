d816dfaaf467080f6d0586a96a0a1e08f5f3c59f
Patrick Boe <patrickboe@gmail.com>
2012-03-03 10:47:19 -0500

    using a different approach for abundant summing

--- p23.rkt
+++ p23.rkt
@@ -1,33 +1,24 @@
 #lang racket
 
-(require srfi/45 "lazy.rkt" "factors.rkt" "streams.rkt")
+(require "factors.rkt" "streams.rkt")
 
 (define (abundant? x)
   (> (apply + (proper-divisors x)) x))
 
 (define abundant-numbers
   (stream-filter abundant? (in-naturals 1)))
-(define (sums-with lst x)
-  (map (λ(a) (+ x a)) lst))
 
-(define (sums s)
-  (lazy
-    (match s
-           ['() '()]
-           [(cons x xs)
-            (append-lazy (sums-with s x) (sums xs))])))
+(define (sum-of-non-abundant-sums-under limit)
+  (local
+    [(define abunds-list (stream->list
+       (stream-take-while (λ(x) (< x limit)) abundant-numbers)))
+     (define abunds-set (list->set abunds-list))
+     (define (non-abundant-sum? x)
+       (not (ormap
+              (λ(y) (set-member? abunds-set (- x y)))
+              abunds-list)))
+     (define non-abundant-sums
+       (stream-filter non-abundant-sum?  (in-range 1 limit)))]
+    (stream-fold + 0 non-abundant-sums)))
 
-(define (abundant-sums-under x)
-  (let ((legal? (λ(y) (< y x))))
-  (lazy->set
-    (filter-lazy
-      legal?
-      (sums (stream->list (stream-take-while legal? abundant-numbers)))))))
-
-(time
-  (define non-abundant-sums
-    (set-subtract
-      (list->set (stream->list (in-range 1 28124)))
-      (abundant-sums-under 28124)))
-
-  (apply + (set->list non-abundant-sums)))
+(time (sum-of-non-abundant-sums-under 21824))
