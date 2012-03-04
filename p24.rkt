ed89954dafb7cc30c69bacc214734d319cab5ff7
Patrick Boe <patrickboe@gmail.com>
2012-03-04 11:01:46 -0500

    solved problem 24

--- p24.rkt
+++ p24.rkt
@@ -1,12 +1,15 @@
-#lang racket
-
-(require "lists.rkt")
+#lang lazy
 
 (define (lex-permutations lst)
+  (reverse-lex-permutations (reverse lst)))
+
+(define (reverse-lex-permutations lst)
   (if (empty? (cdr lst))
     (list lst)
-    (apply append
+    (foldl append '()
       (map (λ(a)
         (map (λ(l) (cons a l))
-          (lex-permutations (remove a lst))))
+          (reverse-lex-permutations (remove a lst))))
       lst))))
+
+(display (list-ref (lex-permutations '(0 1 2 3 4 5 6 7 8 9)) 999999))
