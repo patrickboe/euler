3903ed44bd9ddba5fe6a947166b277a195dddf36
Patrick Boe <patrickboe@gmail.com>
2013-06-29 09:14:33 -0400

    internalizing recursive step

--- p26.scala
+++ p26.scala
@@ -1,17 +1,16 @@
 def decRemainders(x: Int) = (Stream iterate 1){r => (r*10) % x}
 
-def repeatLength(rems: Seq[Int], places: Map[Int,Int], place: Int) : Int = {
-  val a = rems.head
-  places get a match {
-    case Some(i) => place - i
-    case None => repeatLength(rems.tail, places + (a->place), place + 1)
+def period(x: Int) = {
+  def p(rems: Seq[Int], places: Map[Int,Int], place: Int) : Int = {
+    val a = rems.head
+    places get a match {
+      case Some(i) => place - i
+      case None => p(rems.tail, places + (a->place), place + 1)
+    }
   }
+  p(decRemainders(x), Map(), 0)
 }
 
-def inverseRepeatLength(x: Int) = repeatLength(decRemainders(x), Map(), 0)
-
-def maxRepeatsBelow(n: Int) = (2 to n) maxBy inverseRepeatLength
-
-println(maxRepeatsBelow(1000))
+println((2 to 1000) maxBy period)
 
 // vim: set ts=2 sw=2 et:
