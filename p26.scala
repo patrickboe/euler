e98c1369940758390756ca527aa131c603cf26ea
Patrick Boe <patrickboe@gmail.com>
2013-06-28 20:50:23 -0400

    got rid of a compile warning

--- p26.scala
+++ p26.scala
@@ -1,29 +1,9 @@
-/*
-to find the decimal representation of 1/x,
-from k = (-5 to 5) * (10 ^ -j) + i, multiply
-k by x and choose the k with the smallest
-difference from 1 to be the next i, and choose
-j to be the increment of j. yield 0 when the
-difference is zero.
-
-let d be the last digit in the current approximation
-if the approximation is too low, and the
-last digit - 1 if the approximation is too high.
-
-keep a map M of digits 0-9 to how many digits back
-that digit last appeared, along with what the
-error term on the approximation was at that time.
-
-if M[d] has the same error term as the current error,
-this is the beginning of a repeat. yield the distance
-stored for M[d].
-*/
 object Main {
 
   case class ApproximationState(digit: Int, error: Int)
 
   def main(args: Array[String]) =
-    println(maxRepeatsBelow(args.first.toInt))
+    println(maxRepeatsBelow(args.head.toInt))
 
   def maxRepeatsBelow(n: Int) =
     Iterator.range(2,n).maxBy(inverseRepeatLength)
@@ -49,10 +29,9 @@ object Main {
     else {
       val a = approxes.head
       val stateIndex = a.error * 20 + a.digit
-      val occurrence = places.get(stateIndex)
-      occurrence match {
+      places.get(stateIndex) match {
         case Some(i) =>
-           place - i
+          place - i
         case None =>
           repeatLength(
             approxes.tail,
