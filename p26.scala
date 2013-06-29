0a7c83012631f12c104b930ab4ae8607fabba3bd
Patrick Boe <patrickboe@gmail.com>
2013-06-29 08:09:03 -0400

    pared down

--- p26.scala
+++ p26.scala
@@ -1,63 +1,17 @@
-object Main {
+def decRemainders(x: Int) = (Stream iterate 1){r => (r*10) % x}
 
-  case class ApproximationState(digit: Int, error: Int)
-
-  def main(args: Array[String]) =
-    println(maxRepeatsBelow(args.head.toInt))
-
-  def maxRepeatsBelow(n: Int) =
-    Iterator.range(2,n).maxBy(inverseRepeatLength)
-
-  def inverseRepeatLength(x: Int) =
-    repeatLength(
-      imperfectInverseApproximations(x),
-      Map.empty[Int,Int],
-      0)
-
-  def imperfectInverseApproximations(x: Int) =
-    approximationsFrom(x, ApproximationState(0,1)).takeWhile(s=>s.error != 0)
-
-  def decimalFractionError(denom: Int, target: Int, approx: Int) =
-    target - approx * denom
-
-  def repeatLength(
-    approxes: Seq[ApproximationState],
-    places: Map[Int,Int],
-    place: Int) : Int = {
-    if(approxes.tail.isEmpty)
-      0
-    else {
-      val a = approxes.head
-      val stateIndex = a.error * 20 + a.digit
-      places.get(stateIndex) match {
-        case Some(i) =>
-          place - i
-        case None =>
-          repeatLength(
-            approxes.tail,
-            places.updated(stateIndex,place),
-            place + 1)
-      }
-    }
+def repeatLength(rems: Seq[Int], places: Map[Int,Int], place: Int) : Int = {
+  val a = rems.head
+  places get a match {
+    case Some(i) => place - i
+    case None => repeatLength(rems.tail, places + (a->place), place + 1)
   }
-
-  def approximationsFrom(denom: Int, cur: ApproximationState)
-      : Stream[ApproximationState] = {
-    cur #::
-    {
-      val candidateApproxes = Iterator.range(-5,6)
-      val target = cur.error * 10
-      val approx = candidateApproxes.minBy(c =>
-        math.abs(decimalFractionError(denom, target, c))
-      )
-      val nextState = ApproximationState(
-        approx,
-        decimalFractionError(denom, target, approx)
-      )
-      approximationsFrom(denom, nextState)
-    }
-  }
-
 }
+
+def inverseRepeatLength(x: Int) = repeatLength(decRemainders(x), Map(), 0)
+
+def maxRepeatsBelow(n: Int) = (2 to n) maxBy inverseRepeatLength
+
+println(maxRepeatsBelow(1000))
 
 // vim: set ts=2 sw=2 et:
