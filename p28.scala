78f3d02180cae4bafd04d6c669600772df1a3604
Patrick Boe <patrickboe@gmail.com>
2013-07-05 10:12:53 -0400

    tentative solution for 28

---
+++ p28.scala
@@ -0,0 +1,14 @@
+val ringWidths = 2 to 1000 by 2
+
+val ringPerims = ringWidths map (_ * 4)
+
+def tupleSum(t: Tuple2[Int,Int]) = t._1 + t._2
+
+val northeast: Stream[Int] = 1 #:: northeast zip ringPerims map tupleSum
+
+val southeast: Stream[Int] = 1 #:: northeast zip ringWidths map tupleSum
+
+val diagSum = ((northeast tail) ++ (southeast tail) reduce (_ + _)) * 2 + 1
+
+println(diagSum)
+// vim: set ts=2 sw=2 et:
