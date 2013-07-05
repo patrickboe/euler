05c5d678c3fbb6d317101de0166bb5211536177c
Patrick Boe <patrickboe@gmail.com>
2013-07-05 15:25:06 -0400

    fixing an application order issue

--- p28.scala
+++ p28.scala
@@ -4,11 +4,11 @@ val ringPerims = ringWidths map (_ * 4)
 
 def tupleSum(t: Tuple2[Int,Int]) = t._1 + t._2
 
-val northeast: Stream[Int] = 1 #:: northeast zip ringPerims map tupleSum
+val northeast: Stream[Int] = 1 #:: (northeast zip ringPerims map tupleSum)
 
-val southeast: Stream[Int] = 1 #:: northeast zip ringWidths map tupleSum
+val southeast = northeast zip ringWidths map tupleSum
 
-val diagSum = (northeast ++ southeast reduce (_ + _)) * 2 + 1
+val diagSum = (northeast ++ southeast reduce (_ + _)) * 2 - 1
 
 println(diagSum)
 // vim: set ts=2 sw=2 et:
